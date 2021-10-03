205034b04f042a407ab1690532590ba0c89334ad
Steven Zeng <zengsqi199@gmail.com>
2021-10-03 21:31:53 +0800

    add model

---
+++ GithubUserList-SwiftUI/Model/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  GithubUserList-SwiftUI
+//
+//  Created by Steven Zeng on 2021/10/3.
+//
+
+import Foundation
+
+struct User: Codable, Equatable {
+  let login: String
+  let id: Int
+  let avatarURL: URL
+  let type: String
+
+  enum CodingKeys: String, CodingKey {
+    case login
+    case id
+    case avatarURL = "avatar_url"
+    case type
+  }
+}
---
+++ GithubUserList-SwiftUI/Model/UserDetail.swift
@@ -0,0 +1,36 @@
+//
+//  UserDetail.swift
+//  GithubUserList-SwiftUI
+//
+//  Created by Steven Zeng on 2021/10/3.
+//
+
+import Foundation
+
+struct UserDetail: Codable {
+    let login: String
+    let id: Int
+    let avatarURL: URL
+    let type: String
+    let name: String?
+    let bio: String?
+    let location: String?
+    let blog: String
+    let email: String
+    let followers: Int
+    let following: Int
+
+    enum CodingKeys: String, CodingKey {
+        case login
+        case id
+        case avatarURL = "avatar_url"
+        case type
+        case name
+        case bio
+        case location
+        case blog
+        case email
+        case followers
+        case following
+    }
+}
