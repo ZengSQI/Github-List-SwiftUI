8d88ae0b5f3f3cbeacb570435f05cbb11df24ad4
Steven Zeng <zengsqi199@gmail.com>
2021-10-03 21:04:03 +0800

    Initial Commit

---
+++ GithubUserList-SwiftUI/GithubUserList_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubUserList_SwiftUIApp.swift
+//  GithubUserList-SwiftUI
+//
+//  Created by Steven Zeng on 2021/10/3.
+//
+
+import SwiftUI
+
+@main
+struct GithubUserList_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
